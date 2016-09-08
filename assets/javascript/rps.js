25d4e19521d871e9bff1840dc0f229d0c2599cb6
Matt Davis <matt_davis@me.com>
2016-09-08 10:53:48 -0500

    Full game working via sessionStorage (very brittle)

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -8,9 +8,9 @@ var config = {
 firebase.initializeApp(config);
 
 // Executes on every update to Firebase data
-firebase.database().ref().on("value", function(snapshot) {
-	let p1active = snapshot.child("players/1/name").exists();
-	let p2active = snapshot.child("players/2/name").exists();
+firebase.database().ref("players").on("value", function(snapshot) {
+	let p1active = snapshot.child("1/name").exists();
+	let p2active = snapshot.child("2/name").exists();
 
 	if (!game.started) {
 		if (!sessionStorage.getItem("player")) {
@@ -31,18 +31,24 @@ firebase.database().ref().on("value", function(snapshot) {
 	game.started = true;
 
 	if (p1active) {
-		$("#name_p1" ).html(snapshot.child("players/1/name").val());
-		$("#count_p1").html("Wins: " + snapshot.child("players/1/wins").val() + " Losses: " + snapshot.child("players/1/losses").val()).addClass("vis");
+		game.player1Name = snapshot.child("1/name").val();
+		$("#name_p1").html(snapshot.child("1/name").val());
+		$("#count_p1").html("Wins: " + snapshot.child("1/wins").val() + " Losses: " + snapshot.child("1/losses").val()).removeClass("invis");
 	}
 
 	if (p2active) {
-		$("#name_p2" ).html(snapshot.child("players/2/name").val());
-		$("#count_p2").html("Wins: " + snapshot.child("players/2/wins").val() + " Losses: " + snapshot.child("players/2/losses").val()).addClass("vis");
+		game.player2Name = snapshot.child("2/name").val();
+		$("#name_p2").html(snapshot.child("2/name").val());
+		$("#count_p2").html("Wins: " + snapshot.child("2/wins").val() + " Losses: " + snapshot.child("2/losses").val()).removeClass("invis");
 	}
 
-	if ((p1active && sessionStorage.getItem("player") == 1) || (p2active && sessionStorage.getItem("player") == 2)) {
-		game.chatEnabled = true;
+	if (p1active && p2active && !game.playersReady) {
+		game.playersReady = true;
+		game.init();
 	}
+
+	if (snapshot.child("1/choice").exists()) { game.playerTurn(2); }
+	if (snapshot.child("winner").exists()) { game.showWinner(); }
 
 	// If any errors are experienced, log them to console. 
 }, function (errorObject) {
@@ -62,13 +68,78 @@ firebase.database().ref("chat").on("child_added", function(snapshot) {
 // Game object
 let game = {
 	// Properties
-	chatEnabled: false,
+	player1Name: "",
+	player2Name: "",
+	playersReady: false,
 	rpsArr: ["Rock", "Paper", "Scissors"],
 	started: false,
 	
 	// Methods
 	init: function(data){
-		
+		game.playerTurn(1);
+	},
+
+	playerTurn : function(player){
+		if (sessionStorage.getItem("player") == player) {
+			$(".rps-p" + player).removeClass("invis");
+			$("#game_status").html("<h4>It's your turn!<h4>");
+		} else {
+			let otherPlayerName = sessionStorage.getItem("player") == 1 ? game.player2Name : game.player1Name;
+
+			$(".rps-p" + sessionStorage.getItem("player")).addClass("invis");
+			$("#game_status").html("<h4>Waiting for " + otherPlayerName + " to choose.</h4>");
+		}
+	},
+
+	// playerWaiting: function(player){
+	// 	if (sessionStorage.getItem("player") == player) {
+	// 		let otherPlayerName = player == 1 ? game.player2Name : game.player1Name;
+
+	// 		$(".rps-p" + player).addClass("invis");
+	// 		$("#game_status").html("<h4>Waiting for " + otherPlayerName + " to choose.</h4>");
+	// 	}
+	// },
+
+	getWinner: function(player2Choice){
+		firebase.database().ref("players").once("value").then(function(snapshot){
+			if (snapshot.child("1/choice").val() == player2Choice) {
+				firebase.database().ref("players/winner").set("0");
+
+			} else if ((snapshot.child("1/choice").val() == "rock" && player2Choice == "scissors") || 
+				(snapshot.child("1/choice").val() == "paper" && player2Choice == "rock") || 
+				(snapshot.child("1/choice").val() == "scissors" && player2Choice == "paper")) {
+
+				firebase.database().ref("players/1/wins").transaction(function(currentWins) { return currentWins + 1; });
+				firebase.database().ref("players/2/losses").transaction(function(currentLosses) { return currentLosses + 1; });
+
+				firebase.database().ref("players/winner").set("1");
+
+			} else {
+				firebase.database().ref("players/2/wins").transaction(function(currentWins) { return currentWins + 1; });
+				firebase.database().ref("players/1/losses").transaction(function(currentLosses) { return currentLosses + 1; });
+
+				firebase.database().ref("players/winner").set("2");
+			}
+		});
+	},
+
+	showWinner: function(){
+		firebase.database().ref("players").once("value").then(function(snapshot){
+			switch (snapshot.child("winner").val()) {
+				case "0": $("#winner").html("<h2>It's a tie!</h2>"); break;
+				case "1": $("#winner").html("<h2>" + snapshot.child("1/name").val() + " wins!</h2>"); break;
+				case "2": $("#winner").html("<h2>" + snapshot.child("2/name").val() + " wins!</h2>"); break;
+			}
+		});
+
+		setTimeout(resetGame, 2000);
+
+		function resetGame(){
+			$("#winner").html("");
+			firebase.database().ref("players/1/choice").remove();
+			firebase.database().ref("players/winner").remove();
+			game.init();
+		}
 	}
 };
 
@@ -98,10 +169,10 @@ $(function() {
 
 	// Adds text to chat
 	$("#chat_send").on("click", function(event){
-		if (game.chatEnabled) {
-			firebase.database().ref().once("value").then(function(snapshot) {
+		if (game.playersReady) {
+			firebase.database().ref("players").once("value").then(function(snapshot) {
 				let playerNum = sessionStorage.getItem("player");
-				let playerName = snapshot.child("players/" + playerNum + "/name").val();
+				let playerName = snapshot.child(playerNum + "/name").val();
 				let textColor = playerNum == 1 ? "green" : "purple";
 
 				firebase.database().ref("chat").push("<span style='color: " + textColor + ";'>" + playerName + ": " + $("#chat_input").val().trim() + "</span><br/>");
@@ -116,5 +187,13 @@ $(function() {
 		}
 	});
 
-	// game.init();
+	// Picks rock/paper/scissors choice for player 1
+	$(".rps-p1").on("click", function(event){
+		firebase.database().ref("players/1/choice").set($(this).data("choice"));
+	});
+
+	// Picks rock/paper/scissors choice for player 2
+	$(".rps-p2").on("click", function(event){
+		game.getWinner($(this).data("choice"));
+	});
 });
